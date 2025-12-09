19dc4cc9c2565133402caafd46f6435248c3859b
patoworldtv <152646049+patoworldtv@users.noreply.github.com>
2025-12-10 01:59:58 +0200

    Delete v0.25 directory

--- v0.25/src/Timer.cpp
+++
@@ -1,8 +0,0 @@
-#include "Timer.h"
-
-Timer::Timer() : start_(std::chrono::high_resolution_clock::now()) {}
-void Timer::reset() { start_ = std::chrono::high_resolution_clock::now(); }
-long long Timer::ms() const {
-    using namespace std::chrono;
-    return duration_cast<milliseconds>(high_resolution_clock::now() - start_).count();
-}
